98d54474d44e0112c6d7eec54ea11705c314459e
Supaluck Singjan <supalucksingjan@gmail.com>
2026-03-20 23:01:14 +0700

    Add post fomats array

--- functions.php
+++ functions.php
@@ -22,6 +22,6 @@ add_theme_support('custom-background');
 add_theme_support('custom-header');
 add_theme_support('post-thumbnails');
 
-add_theme_support('post-formats');
+add_theme_support('post-formats', array('aside', 'image', 'video'));
 
 ?>
