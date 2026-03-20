e1a2d841e37b245d02eaaf7e919bfc976a3e0db7
Supaluck Singjan <supalucksingjan@gmail.com>
2026-03-20 22:56:46 +0700

    Post Formats

--- functions.php
+++ functions.php
@@ -20,4 +20,8 @@ add_action('after_setup_theme', 'awesome_theme_setup');
 
 add_theme_support('custom-background');
 add_theme_support('custom-header');
+add_theme_support('post-thumbnails');
+
+add_theme_support('post-formats');
+
 ?>
