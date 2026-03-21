65ebce741e62af34d2db43329eae1f6586d53d37
Supaluck Singjan <supalucksingjan@gmail.com>
2026-03-21 23:58:42 +0700

    Add get post format

---
+++ content-image.php
@@ -0,0 +1,4 @@
+<h3>Image Post<?php the_title(); ?> </h3>
+<div class="thumbnail-img"><?php the_post_thumbnail(); ?></div><small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g: i a'); ?>, in <?php the_category(); ?></small>
+
+<hr/>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if(have_posts()):
     
     while( have_posts()): the_post(); echo "This is post format"; ?>
 
-       <?php get_template_part('content', 'get_post_format()'); ?>
+       <?php get_template_part('content', get_post_format()); ?>
 
     
     <?php endwhile;
