b367908309d2c1ea710b7651b0905d857eef45c3
Supaluck Singjan <supalucksingjan@gmail.com>
2026-03-22 15:41:35 +0700

    Add comments

--- functions.php
+++ functions.php
@@ -1,15 +1,25 @@
 <?php 
 
-// Add CSS and JavaScript
+/*
+=============================
+ Include scripts
+=============================
+*/
+
 function awesome_script_enqueue() {
     wp_enqueue_style('customstyle', get_template_directory_uri().'/css/awesome.css', array(), '1.0.0', 'all');
     wp_enqueue_script('customsscript', get_template_directory_uri().'/js/awesome.js', array(), '1.0.0', true);
+    wp_enqueue_script('jquery');
 }
 
 add_action('wp_enqueue_scripts', 'awesome_script_enqueue');
 
 
-// Add menu 
+/*
+=============================
+ Activate menus
+=============================
+*/
 function awesome_theme_setup() {
     add_theme_support('menus');
     register_nav_menu('primary', 'Primary Header Navigation');
@@ -18,6 +28,11 @@ function awesome_theme_setup() {
 
 add_action('after_setup_theme', 'awesome_theme_setup');
 
+/*
+=============================
+ Theme support function 
+=============================
+*/
 add_theme_support('custom-background');
 add_theme_support('custom-header');
 add_theme_support('post-thumbnails');
