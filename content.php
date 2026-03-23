598d2b326434aa910d23fc834e0894903531f999
Supaluck Singjan <supalucksingjan@gmail.com>
2026-03-23 23:03:40 +0700

    Add post_class().

--- content.php
+++ content.php
@@ -1,5 +1,5 @@
 
-<article id="post-<?php the_ID(); ?>" class="">
+<article id="post-<?php the_ID(); ?>" class="<?php post_class(); ?>">
 
        <h3> <?php the_title(); ?> </h3>
        <div class="thumbnail-img"><?php the_post_thumbnail(); ?></div>
