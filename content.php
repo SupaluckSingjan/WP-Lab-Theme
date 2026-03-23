2911ac69b644ae3d5346560cea686dc0588114c1
Supaluck Singjan <supalucksingjan@gmail.com>
2026-03-23 22:49:56 +0700

    Add the_ID() to a post.

--- content.php
+++ content.php
@@ -1,6 +1,7 @@
 
+<article id="post-<?php the_ID(); ?>" class="">
 
-<h3> <?php the_title(); ?> </h3>
+       <h3> <?php the_title(); ?> </h3>
        <div class="thumbnail-img"><?php the_post_thumbnail(); ?></div>
        <small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g: i a'); ?>, in <?php the_category(); ?></small>
 
@@ -8,3 +9,4 @@
 
         <hr/>
 
+</article>
