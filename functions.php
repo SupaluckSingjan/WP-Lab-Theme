7e8a4f6f323204fa9e58d5713190f3879879ccd8
Supaluck Singjan <supalucksingjan@gmail.com>
2026-04-29 23:39:31 +0700

    Add Sidebar

--- functions.php
+++ functions.php
@@ -53,6 +53,12 @@ function awesome_widget_setup() {
     register_sidebar(array(
         'name'=> 'Sidebar',
         'id' => 'sidebar-1',
+        'class' => 'custom',
+        'description' => 'Standard Sidebar',
+        'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        'after_widget'  => "</li>\n",
+        'before_title'  => '<h2 class="widgettitle">',
+        'after_title'   => "</h2>\n",
     ));
 }
 
